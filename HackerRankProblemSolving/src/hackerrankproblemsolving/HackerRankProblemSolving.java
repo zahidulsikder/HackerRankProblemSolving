912813ea79bb1b939c8af23568bdd9a7c2407f00
zahidulsikder <mdzahidulsikder@gmail.com>
2023-12-13 09:16:34 +0600

    Print  lines of output; each line  (where ) contains the  of  in the form: N x i = result.

--- HackerRankProblemSolving/src/hackerrankproblemsolving/HackerRankProblemSolving.java
+++ HackerRankProblemSolving/src/hackerrankproblemsolving/HackerRankProblemSolving.java
@@ -1,30 +1,24 @@
 
 package hackerrankproblemsolving;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class HackerRankProblemSolving {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("======================");
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
         
-        for(int i =0; i<3 ; i++){
-            String s1 = sc.next();
-            int x = sc.nextInt();
-             System.out.printf("%-14s %03d%n",s1,x);
-            
-            
+        for(int i = 1; i<=10; i ++){
+            System.out.println(N+ " x " +i + " = " + i*N);
         }
-        System.out.print("======================");
-            sc.close();
         
-     
-           
-     
-      
-    
-        
+
+        bufferedReader.close();
         
     }
     
