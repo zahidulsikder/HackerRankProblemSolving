1c4d545ddefbd94d1841d4997513b3c63a7fa8cb
zahidulsikder <mdzahidulsikder@gmail.com>
2023-12-13 09:02:40 +0600

    14 car string and 3 digit int value output

---
+++ HackerRankProblemSolving/src/hackerrankproblemsolving/HackerRankProblemSolving.java
@@ -0,0 +1,33 @@
+
+package hackerrankproblemsolving;
+
+import java.util.Scanner;
+
+public class HackerRankProblemSolving {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("======================");
+        
+        for(int i =0; i<3 ; i++){
+            String s1 = sc.next();
+            int x = sc.nextInt();
+             System.out.printf("%-14s %03d%n",s1,x);
+            
+            
+        }
+        System.out.print("======================");
+            sc.close();
+        
+     
+           
+     
+      
+    
+        
+        
+    }
+    
+    
+    
+}
